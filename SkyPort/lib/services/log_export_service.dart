70a590126e593aed99a887d3e71001280aa19960
starsphere <starsphere@foxmail.com>
2026-03-13 01:38:13 +0800

    refactor: remove unnecessary imports
    - Remove redundant flutter/foundation.dart import (exported by material.dart)
    - Remove unused dart:convert import from test file
    
    No issues found in flutter analyze, all 360 tests passing

--- SkyPort/lib/services/log_export_service.dart
+++ SkyPort/lib/services/log_export_service.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:intl/intl.dart';
 
